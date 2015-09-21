cdaf602caec21118566d9327c4b2ff53ad85f95c
Doelia <kyxalia@gmail.com>
2015-09-21 13:56:34 +0200

    ajout autre personne

--- eclipse/project1/src/project1_1/PrincipalImpot.java
+++ eclipse/project1/src/project1_1/PrincipalImpot.java
@@ -4,14 +4,26 @@ public class PrincipalImpot {
 	
 	public static void main(String[] params) {
 		Revenu revenu1 = new Revenu(10, 0, 5); 
-		Imposable impos1 = new Imposable("Dupond", "Bernard", 345, 2, null, 3, revenu1);
-		impos1.setFrais(4); 
-		//impos1.revenu=revenu1;
-		Impot imp= new Impot();
-		double montantImpot = imp.calculerImpot(impos1);
-		impos1.enregistrerImpot(montantImpot);
+		Imposable impos1 = new Imposable("Dupond", "Bernard", 345, 1, null, 3, revenu1);
+		Imposable impos2 = new Imposable("Dupond", "Jean", 346, 2, null, 1, revenu1);
+		
+		{
+			impos1.setFrais(4); 
+			Impot imp = new Impot();
+			double montantImpot = imp.calculerImpot(impos1);
+			impos1.enregistrerImpot(montantImpot);
+			System.out.println(montantImpot); 
+		}
+		
+		{
+			impos2.setFrais(4); 
+			Impot imp = new Impot();
+			double montantImpot = imp.calculerImpot(impos2);
+			impos2.enregistrerImpot(montantImpot);
+			System.out.println(montantImpot); 
+		}
+		
 		(new FeuilleImpot(2)).editer(5);
-		System.out.println(montantImpot); 
 	}
 
 }
